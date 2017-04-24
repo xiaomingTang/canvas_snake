622b361a78d60fad8fd53898f6649c76cf025d40
xiaomingTang <1038761793@qq.com>
2017-04-24 20:52:49 +0800

    修改了配色和移动逻辑，消除了一个隐藏老深老深的bug，该bug会导致食物偶现miss

--- snake.js
+++ snake.js
@@ -7,19 +7,37 @@ window.onload=function(){
  	function unbind(arr){
 		return [arr[0],arr[1]];
 	}
- 	function Map(){
-		this.width=50;
-		this.height=40;
+	
+	
+	//	取得屏幕的宽高并赋给canvas
+	var sx = document.documentElement.clientWidth || document.body.clientWidth;
+	var	sy = document.documentElement.clientHeight || document.body.clientHeight;
+	var w = sx > sy ? sy * 0.95 : sx * 0.95;
+	var canvas = $("snake");
+	canvas.width = canvas.height = w;
+	canvas.style.marginTop = (sx > sy ? sy * 0.025 : sx * 0.025) + "px";
+	//	config作为全局变量，因为输了之后的重新初始化需要用到
+	var config = {
+		canvas: canvas,
+		width: w,
+		height: w
+	};
+	
+	
+ 	function Map(config){
+		this.ctx = config.canvas.getContext ? config.canvas.getContext("2d") : null;
+		this.width = config.width / 10;
+		this.height = config.height / 10;
+		
 		this.available={};	//可用点, 不含蛇头蛇身, 一维对象
 		this.head=[];		//蛇头, 一维数组
 		this.body=[];		//蛇体, 二维数组
 		this.food=[];		//食物, 一维数组
-		this.ctx= $("snake").getContext ? $("snake").getContext("2d") : null;		//作图上下文
 	}
 	Map.prototype={
 		constructor: Map,
 		init: function(){
-			this.ctx.clearRect(0,0,500,500);
+			this.ctx.clearRect(0,0,this.width*10,this.height*10);
 			//构建地图
 			for(var i=0,leni=this.width;i<leni;i++){
 				for(var j=0,lenj=this.height;j<lenj;j++){
@@ -47,6 +65,7 @@ window.onload=function(){
 					pos=key.split("_");
 				this.food[0]=parseInt(pos[0]);
 				this.food[1]=parseInt(pos[1]);
+				console.log(this.food[0] + "---" + this.food[1]);
 				delete this.available[key];
 			}
 			else{
@@ -57,31 +76,31 @@ window.onload=function(){
 		clear: function(arr){
 			var x=arr[0],y=arr[1];
 			this.ctx.clearRect(10*x,10*y,10,10);
-			this.render(x,y,"#666");
+			this.render(x,y,"#666");	//	background，清除的时候和背景色不同，可以显示轨迹，意外之喜哦。。。
 		},
 		render: function(x,y,color){
 			this.ctx.fillStyle=color;
-			this.ctx.strokeStyle= "#ddd";
+			this.ctx.strokeStyle= "#5d5d5d";	//	border
 			this.ctx.fillRect(10*x,10*y,10,10);
 			this.ctx.strokeRect(10*x,10*y,10,10);
 		},
 		renderMap: function(){
 			for(var i=0,leni=this.height;i<leni;i++){
 				for(var j=0,lenj=this.width;j<lenj;j++){
-					this.render(j,i,"#666");
+					this.render(j,i,"#636363");	//	background
 				}
 			}
 		},
 		renderHead: function(){
-			this.render(this.head[0],this.head[1],"red");
+			this.render(this.head[0],this.head[1],"#c34");	//	head
 		},
 		renderBody: function(){
 			for(var i=0,len=this.body.length;i<len;i++){
-				this.render(this.body[i][0],this.body[i][1],"pink");
+				this.render(this.body[i][0],this.body[i][1],"#cab");	//	body
 			}
 		},
 		renderFood: function(){
-			this.render(this.food[0],this.food[1],"blue");
+			this.render(this.food[0],this.food[1],"#78c");	//	food
 		},
 		//param n: 37--left  38--top  39--right  40--bottom ;
 		move: function(n){
@@ -103,18 +122,17 @@ window.onload=function(){
 				default:
 					return this;
 			}
-			this.head=unbind(nextHead);
-			
 			if( !(nextHead[0]+"_"+nextHead[1] in this.available) && (nextHead.toString() != this.food.toString())){
 				clearInterval(loop);
 				loop=undefined;
 				dir=0;
 				alert("胜败乃兵家常事 大侠请重新来过");
-				this.ctx.clearRect(0,0,500,500);
-				map=new Map();
+				this.ctx.clearRect(0,0,this.width*10,this.height*10);
+				map=new Map(config);
 				map.init();
 				return this;
 			}
+			this.head=unbind(nextHead);
 			delete this.available[nextHead[0]+"_"+nextHead[1]];
 			this.clear(preHead);
 			for(var i=0,len=this.body.length;i<len;i++){
@@ -124,8 +142,8 @@ window.onload=function(){
 			if(this.head.toString() != this.food.toString()){
 				var tail=this.body.pop();
 				this.available[tail[0]+"_"+tail[1]]=1;
-			}
-			else{
+			} else {
+				delete this.available[preHead[0]+"_"+preHead[1]];
 				this.randFood();
 				this.renderFood();
 			}
@@ -139,31 +157,36 @@ window.onload=function(){
 		
 	}
  	
-	var map=new Map(),
-		dir=0,
-		loop=undefined,
-		lastPos=[];
- 	if(!map.ctx){
+	var dir = 0;
+	var loop = undefined;
+	var lastPos = [];
+	var map = new Map(config);
+	if(!map.ctx){
 		alert("你的浏览器不支持canvas,所以你看不到这个页面,请升级浏览器至最新版本,并:该页面即将关闭!");
 		window.close();
 	}
 	
+	alert("键盘方向键 或 移动端滑屏 控制方向，空格键暂停，暂停之后别往回走哦，会狗带的。ok, have a nice day ! enjoy the game !");
+	
 	map.init();
 	
-	//键盘事件绑定
+	//	键盘事件绑定
 	document.onkeydown=function(e){
-		var event=e || window.event;
-		if(event.keyCode-dir==2 || event.keyCode-dir==-2){
+		var event = e || window.event;
+		var temp = event.keyCode;
+		if(temp-dir==2 || temp-dir==-2){
 			return false;
+		} else if(temp === 32 || (temp.toString() in {"37":0,"38":0,"39":0,"40":0})){
+			dir=temp;
 		}
-		dir=event.keyCode;
 		if(typeof loop === "undefined" && (dir.toString() in {"37":0,"38":0,"39":0,"40":0})){
 			loop=setInterval(function(){
 				map.move(dir);
-			},200);
+			},100);
 		}
 	}
-	//触摸事件绑定
+	
+	//	触摸事件及绑定
 	function handleTouchEvent(event){
 		if(event.touches.length==1){
 			switch(event.type){
@@ -194,7 +217,6 @@ window.onload=function(){
 			
 		}
 	}
-	
 	document.ontouchstart=handleTouchEvent;
 	document.ontouchmove=handleTouchEvent;
 	
